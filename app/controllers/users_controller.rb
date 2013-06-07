2e775c39aebb5f68134156871a41cbad265e981e
binli <zigzibet@hotmail.com>
2013-06-07 15:25:20 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-     redirect_to @user
+      redirect_to @user
     else
       render 'new'
     end    
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 before_save { self.email = email.downcase }
+before_save :create_remember_token
 	
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -9,4 +10,11 @@ before_save { self.email = email.downcase }
 has_secure_password
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+
+   private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
